86ca5083526afbdf3afc1287be46418fbf85f624
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-06-12 05:28:50 -0300

    add verify userexist and email duplicate

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -26,6 +26,44 @@ class UsersController {
 
         return response.status(201).json();
     }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get('SELECT * FROM users WHERE id = (?)', [
+            id,
+        ]);
+
+        if (!user) {
+            throw new AppError('Usuário não encontrado.');
+        }
+
+        const userWithUpdatedEmail = await database.get(
+            'SELECT * FROM users WHERE email = (?)',
+            [email]
+        );
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError('Este email já está em uso.');
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(
+            `
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return response.json();
+    }
 }
 
 module.exports = UsersController;
